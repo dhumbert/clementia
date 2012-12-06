fea46f793af463be2dd749ec1045c106e7209cfc
Devin Humbert <devin.humbert@gmail.com>
2012-12-06 14:28:32 -0800

    We don't actually need dojo/query here.

--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,5 @@
 // set up Dojo Bootstrap JS
-require(["bootstrap/Alert", "bootstrap/Dropdown", "dojo/query"]);
+require(["bootstrap/Alert", "bootstrap/Dropdown"]);
 
 // allow faking of request method
 require(["clementia/fake-http-method"]);
