e8ab2e0702131864e3874847db43a0e91e7bd63f
Devin Humbert <devin.humbert@gmail.com>
2013-04-11 19:14:35 -0700

    Reset pass form

--- application/views/user/forgot_password_reset.php
+++ application/views/user/forgot_password_reset.php
@@ -2,6 +2,9 @@
     <div class="row">
         <div class="span6 offset3">
             <h2>Reset Your Password</h2>
+            <?php echo Form::open(NULL, 'POST'); ?>
+                <?php echo Form::token(); ?>
+            <?php echo Form::close(); ?>
         </div>
     </div>
 <?php Section::stop(); ?>
