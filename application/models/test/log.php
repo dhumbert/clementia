aa8117be2b5b233a62f92dbf63fd06e635aa30c5
Devin Humbert <devin.humbert@gmail.com>
2013-04-11 17:17:57 -0700

    Formatting of one more file

--- application/models/test/log.php
+++ application/models/test/log.php
@@ -3,16 +3,16 @@ namespace Test;
 
 class Log extends \Aware 
 {
-  public static $timestamps = true;
-  public static $table = 'test_logs';
+    public static $timestamps = true;
+    public static $table = 'test_logs';
 
-  public static $rules = array(
-    'test_id' => 'required',
-    'passed' => 'required',
-  );
+    public static $rules = array(
+        'test_id' => 'required',
+        'passed' => 'required',
+    );
 
-  public function test() 
-  {
-    return $this->belongs_to('Test');
-  }
+    public function test() 
+    {
+        return $this->belongs_to('Test');
+    }
 }
