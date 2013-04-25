b8c64ba8aab35025d24cee72e3f2032e9b7e64dc
Devin Humbert <devin.humbert@gmail.com>
2013-04-25 13:47:30 -0700

    Removed some unnecessary markup

--- application/views/user/account.php
+++ application/views/user/account.php
@@ -1,37 +1,23 @@
 <?php Section::start('content'); ?>
-  <div class="row">
-    <div class="span4 offset4 login-form">
-        <h1>Account</h1>
-        <?php echo Form::open('user/index', 'PUT'); ?>
+    <div class="row">
+        <div class="span4 offset4 login-form">
+            <h1>Account</h1>
+            <?php echo Form::open('user/index', 'PUT'); ?>
             <?php echo Form::token(); ?>
 
             <p>Account type: <strong><?php echo $user->role->name; ?></strong></p>
 
-            <div class="control-group">
-              <?php echo Form::label('email', 'Email Address', array('class' => 'control-label')); ?>
-              <div class="controls">
-                <?php echo Form::email('email', Input::old('email', Auth::user()->email), array('class' => 'span4')); ?>
-              </div>
-            </div>
-
-            <div class="control-group">
-              <?php echo Form::label('password', 'Password', array('class' => 'control-label')); ?>
-              <div class="controls">
-                <?php echo Form::password('password', array('class' => 'span4')); ?>
-              </div>
-            </div>
-
-            <div class="control-group">
-              <?php echo Form::label('password_confirmation', 'Confirm Password', array('class' => 'control-label')); ?>
-              <div class="controls">
-                <?php echo Form::password('password_confirmation', array('class' => 'span4')); ?>
-              </div>
-              <span class="help-block">Leave the password fields blank to keep your current password.</span>
-            </div>
+            <?php echo Form::label('email', 'Email Address', array('class' => 'control-label')); ?>
+            <?php echo Form::email('email', Input::old('email', Auth::user()->email), array('class' => 'span4')); ?>
+            <?php echo Form::label('password', 'Password', array('class' => 'control-label')); ?>
+            <?php echo Form::password('password', array('class' => 'span4')); ?>
+            <?php echo Form::label('password_confirmation', 'Confirm Password', array('class' => 'control-label')); ?>
+            <?php echo Form::password('password_confirmation', array('class' => 'span4')); ?>
+            <span class="help-block">Leave the password fields blank to keep your current password.</span>
 
             <button type="submit" class="btn btn-primary">Save</button>
-        <?php echo Form::close(); ?>
+            <?php echo Form::close(); ?>
+        </div>
     </div>
-</div>
 
 <?php Section::stop(); ?>
